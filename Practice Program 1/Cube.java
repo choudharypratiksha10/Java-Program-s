7853587eb3e576327e5fa8aa77c022259bb4852d
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-01-25 00:19:16 +0530

    Practice Program 1
    WAP using class Cube and calculate area of two objects

---
+++ Practice Program 1/Cube.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class MyProgramCube{
+	public static void main(String[] args){
+		float area;
+		Cube c1 = new Cube();
+		Cube c2 = new Cube();
+
+		System.out.println("c1 area = " + c1.calArea(10, 10, 10));
+		System.out.println("c2 area = " + c2.calArea(10, 20, 30));
+	}
+}
+
+class Cube{
+	float height;
+	float width;
+	float depth;
+	
+	float calArea(float h, float w, float d){
+		height = h;
+		width = w;
+		depth = d;
+
+		return height * width * depth;
+	}
+}
+
+ 
