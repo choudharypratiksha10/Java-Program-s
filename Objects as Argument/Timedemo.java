5fe47c1fc02dd8bda093beb344cbda004902e92a
Pratiksha Choudhary <choudharypratiksha36@gmail.com>
2026-01-25 18:52:28 +0530

    Program 17
    Passing Objects as Argument

---
+++ Objects as Argument/Timedemo.java
@@ -0,0 +1,38 @@
+class Time{
+	int hour;
+	int minute;
+	int second;
+
+	Time(int hour, int minutes, int second){
+		this.second = second;
+		this.minute = minutes;
+		this.hour = hour;
+	}
+
+	void add(Time t){
+		this.second += t.second;
+		if (this.second >= 60){
+			this.minute++;
+			this.second -= 60;
+		}
+
+		this.minute += t.minute;
+		if (this.minute >= 60){
+			this.hour++;
+			this.minute -= 60;
+		}
+	
+		this.hour += t.hour;
+	}
+}
+
+public class TimeDemo{
+	public static void main(String[] args){
+		Time t1 = new Time(11, 59, 55);
+		Time t2 = new Time(0, 0, 5);
+
+		t1.add(t2);
+
+		System.out.println(t1.hour + ":" + t1.minute + ":" + t1.second);
+	}
+}		
